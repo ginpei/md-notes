537ce7591c460bbe351c3678f5554584fe4b996b
Takanashi Ginpei <ginpei@gmail.com>
2012-10-01 04:30:00 +0900

    crate user with auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessible :account, :image_url, :provider, :provider_uid
+
+  def self.create_with_auth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.provider_uid = auth['uid']
+
+      info = auth['info']
+      user.account = info['nickname']
+      user.image_url = info['image']
+    end
+  end
 end
