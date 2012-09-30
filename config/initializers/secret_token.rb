f609798e8da827ff0b6061ab6526d35674a3d5ba
Takanashi Ginpei <ginpei@gmail.com>
2012-10-01 03:13:14 +0900

    build rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MdNotes::Application.config.secret_token = '871b0fd03373ba257bea23fd66fffa14b28d120c0b0e4cdd2abcae1a6d46a9fc4409448c40f709d744f31a3007bda0f244a9cca86b0af8a23343d0f954ce4b02'
