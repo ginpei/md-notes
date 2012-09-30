8f51fc1db30c72948550555c1315e19003e7ea78
Takanashi Ginpei <ginpei@gmail.com>
2012-10-01 04:53:36 +0900

    add helper signin_path instead of routing alias

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :signin_path
+  helper_method :current_user
+
+  private
+
+  def signin_path(provider)
+    "#{root_path}auth/#{provider}"
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
