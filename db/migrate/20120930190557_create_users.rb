9592db82c20f0ed6163bd173cf4da40d867ba4bd
Takanashi Ginpei <ginpei@gmail.com>
2012-10-01 04:12:16 +0900

    fix migration to build user table:
     * not null
     * index for account
     * index for provider and its uid

--- db/migrate/20120930190557_create_users.rb
+++ db/migrate/20120930190557_create_users.rb
@@ -1,12 +1,15 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :account
-      t.string :image_url
-      t.string :provider
-      t.string :provider_uid
+      t.string :account, null: false
+      t.string :image_url, null: false
+      t.string :provider, null: false
+      t.string :provider_uid, null: false
 
       t.timestamps
     end
+
+    add_index :users, :account, unique: true
+    add_index :users, [:provider, :provider_uid], unique: true
   end
 end
