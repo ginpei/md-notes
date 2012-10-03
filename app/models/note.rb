a5853eb4ae35999ccf96e5ce90d3ed85540f15f0
Takanashi Ginpei <ginpei@gmail.com>
2012-10-04 02:19:17 +0900

    add the method in User to show title or first line of body

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,4 +5,14 @@ class Note < ActiveRecord::Base
   def self.latest
     order('updated_at DESC').all
   end
+
+  def title_to_show
+    title = self['title']
+    if title && title != ''
+      title
+    else
+      body = self['body']
+      body[0, body.index("\n")]
+    end
+  end
 end
