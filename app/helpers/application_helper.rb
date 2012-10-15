f27e3c129394469cede1da7a92c2e2108305452a
Takanashi Ginpei <ginpei@gmail.com>
2012-10-16 04:50:29 +0900

    add helper to build global header item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def global_header_item(title, contents)
+    html = '<div class="item">'
+    html += '<div class="subject">'
+    html += title
+    html += '</div>'
+    html += '<ul class="content">'
+    contents.each do |content|
+      html += '<li>'
+      html += link_to(content[:label], content[:url])
+      html += '</li>'
+    end
+    html += '</ul>'
+    html += '</div>'
+
+    html.html_safe
+  end
 end
