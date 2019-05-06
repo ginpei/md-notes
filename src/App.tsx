7a3b3b39f4b88433382281f3c37f90a5a0cd847f
ginpei <ginpei@gmail.com>
2019-05-05 18:18:32 -0700

    firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import firebase from './middleware/firebase';
 import AboutPage from './screens/AboutPage';
 import HomePage from './screens/HomePage';
 import NoteWritePage from './screens/NoteWritePage';
 import NotFoundPage from './screens/NotFoundPage';
 
 const App: React.FC = () => {
+  const [initialized, setInitialized] = useState(false);
+  const [user, setUser] = useState<firebase.User | null>(null);
+  useEffect(() => {
+    const auth = firebase.auth();
+    return auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+      setInitialized(true);
+    });
+  }, []);
+
+  if (!initialized) {
+    return (
+      <div>
+        🥚
+        🐣
+        🐥
+      </div>
+    );
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
           <Route path="/" exact component={HomePage} />
           <Route path="/about" component={AboutPage} />
-          <Route path="/notes/:id/write" component={NoteWritePage} />
+          {user && (
+            <>
+              <Route path="/notes/:id/write" component={NoteWritePage} />
+            </>
+          )}
           <Route component={NotFoundPage}/>
         </Switch>
       </div>
---
+++ src/middleware/firebase.ts
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+};
+firebase.initializeApp(config);
+
+export default firebase;
