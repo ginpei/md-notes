9c1afd6618fffef9080278e83c781c086c228465
ginpei <ginpei@gmail.com>
2019-05-09 02:04:29 -0700

    update for cool loading

--- src/screens/InitializingPage.tsx
+++ src/screens/InitializingPage.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Center = styled.div`
+  align-items: center;
+  background-color: #fff9;
+  box-shadow: 0 0 20vmin #0003 inset;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+`;
+
+const Shaking = styled.div`
+  animation: shaking 1000ms infinite;
+
+  @keyframes shaking {
+    0% { transform: translateX( 0  ) translateY( 0  ); }
+    5% { transform: translateX( 2px) translateY( 2px); }
+    10% { transform:                  translateY(-2px); }
+    15% { transform: translateX(-2px) translateY( 2px); }
+    20% { transform:                  translateY(-2px); }
+    25% { transform: translateX( 0  ) translateY( 0  ); }
+    100% { transform: translateX( 0  ) translateY( 0  ); }
+  }
+`;
 
 const InitializingPage: React.FC = () => {
   return (
-    <div>
-      🥚
-      🐣
-      🐥
-    </div>
+    <Center>
+      <Shaking>📝</Shaking>
+    </Center>
   );
 }
 
