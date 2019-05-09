ef4c2c7dbad54c68001f187d83f7844ade73026e
ginpei <ginpei@gmail.com>
2019-05-09 01:06:48 -0700

    update to show note list link only for logged-in user

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -24,12 +24,12 @@ const HomePage: React.FC<PageProps> = (props) => {
         <img src="/icon-512.png" alt="logo" width="256" height="256" />
       </div>
       <ul>
-        {props.loggedIn ? (
+        {props.loggedIn ? (<>
+          <li><Link to={notePath()}>Note list</Link></li>
           <li><Link to="/logout">Logout</Link></li>
-          ) : (
+        </>) : (
           <li><Link to="/login">Login</Link></li>
         )}
-        <li><Link to={notePath()}>Note list</Link></li>
       </ul>
     </AppLayout>
   );
