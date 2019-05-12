b3504beb0ab7413612e7be8370cb26b2d632c6f9
ginpei <ginpei@gmail.com>
2019-05-11 20:12:04 -0700

    extract style attrs to scss file

--- src/independents/Dialog.tsx
+++ src/independents/Dialog.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { HtmlComponent, HtmlProps } from '../misc';
+import './Dialog.scss';
 
 const Dialog: React.FC = ({ children }) => {
   return (
-    <div style={{
-      backgroundColor: '#0003',
-      height: '100vh',
-      left: '0',
-      position: 'fixed',
-      top: '0',
-      width: '100vw',
-    }}>
-      <div style={{
-        backgroundColor: '#fff',
-        borderRadius: '0.2em',
-        height: 'calc(100% - 2rem)',
-        left: '1rem',
-        overflow: 'auto',
-        position: 'absolute',
-        top: '1rem',
-        width: 'calc(100% - 2rem)',
-      }}>
+    <div className="Dialog-outer">
+      <div className="Dialog">
         {children}
       </div>
     </div>
@@ -34,11 +19,7 @@ export const DialogTitle: HtmlComponent<'h1'> = (props) => {
   return (
     <h1
       {...props}
-      className="container"
-      style={{
-        height: '2em',
-        lineHeight: '2em',
-      }}
+      className="container DialogTitle"
     />
   );
 }
@@ -46,23 +27,14 @@ export const DialogTitle: HtmlComponent<'h1'> = (props) => {
 export const DialogHeading: HtmlComponent<'h2'> = (props) => (
   <h2
     {...props}
-    className="container"
-    style={{
-      fontSize: '0.8em',
-      height: '2em',
-      lineHeight: '2em',
-      marginTop: '1em',
-    }}
+    className="container DialogHeading"
   />
 );
 
 export const DialogLink: React.FC<LinkProps> = (props) => (
   <Link
     {...props}
-    className="container Dialog-item"
-    style={{
-      backgroundColor: '#f0f9ff',
-    }}
+    className="container Dialog-item DialogLink"
   />
 );
 
@@ -74,44 +46,13 @@ interface DialogInputProp extends HtmlProps<'input'> {
 export const DialogInput: React.FC<DialogInputProp> = (props) => {
   const { description, label, ...inputProps } = props;
   return (
-    <label
-      className="container Dialog-item"
-      style={{
-        color: '#666',
-        height: 'auto',
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <span
-          style={{
-            marginRight: '0.5em',
-          }}
-        >
-          {label}
-        </span>
-        <input
-          {...inputProps}
-          style={{
-            borderStyle: 'none',
-            textAlign: 'right',
-            width: '100%',
-          }}
-        />
+    <label className="container Dialog-item DialogInput">
+      <div className="DialogInputLine">
+        <span className="DialogInputLabel">{label}</span>
+        <input {...inputProps} className="DialogInnerInput" />
       </div>
       {description && (
-        <div
-          style={{
-            fontSize: '0.8em',
-            fontStyle: 'oblique',
-            height: '1.2em',
-            lineHeight: '1.2em',
-          }}
-        >{description}</div>
+        <div className="DialogInputDescription">{description}</div>
       )}
     </label>
   );
